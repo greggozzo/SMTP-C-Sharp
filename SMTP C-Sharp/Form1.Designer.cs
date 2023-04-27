78d86787ad81ea490d2346b4ec1c23545906a411
Gregory Gozzo WGU <ggozzo@wgu.edu>
2023-04-27 10:16:54 -0700

    Add project files.

---
+++ SMTP C-Sharp/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿
+namespace SMTP_C_Sharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblRegion = new System.Windows.Forms.Label();
+            this.LblFrom = new System.Windows.Forms.Label();
+            this.LblTo = new System.Windows.Forms.Label();
+            this.LblSubject = new System.Windows.Forms.Label();
+            this.LblBody = new System.Windows.Forms.Label();
+            this.lblAtach = new System.Windows.Forms.Label();
+            this.LblAccessKey = new System.Windows.Forms.Label();
+            this.LblSecretKey = new System.Windows.Forms.Label();
+            this.CbRegion = new System.Windows.Forms.ComboBox();
+            this.TxtFrom = new System.Windows.Forms.TextBox();
+            this.TxtTo = new System.Windows.Forms.TextBox();
+            this.TxtSubject = new System.Windows.Forms.TextBox();
+            this.TxtBody = new System.Windows.Forms.TextBox();
+            this.txtAttachment = new System.Windows.Forms.TextBox();
+            this.TxtKey = new System.Windows.Forms.TextBox();
+            this.TxtSecret = new System.Windows.Forms.TextBox();
+            this.TxtFilePath = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.ButtonSend = new System.Windows.Forms.Button();
+            this.ButtonSearch = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.CheckDelegate = new System.Windows.Forms.CheckBox();
+            this.TxtDelegate = new System.Windows.Forms.TextBox();
+            this.ButtonSearchFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LblRegion
+            // 
+            this.LblRegion.AutoSize = true;
+            this.LblRegion.Location = new System.Drawing.Point(26, 41);
+            this.LblRegion.Name = "LblRegion";
+            this.LblRegion.Size = new System.Drawing.Size(44, 13);
+            this.LblRegion.TabIndex = 0;
+            this.LblRegion.Text = "Region:";
+            // 
+            // LblFrom
+            // 
+            this.LblFrom.AutoSize = true;
+            this.LblFrom.Location = new System.Drawing.Point(26, 107);
+            this.LblFrom.Name = "LblFrom";
+            this.LblFrom.Size = new System.Drawing.Size(33, 13);
+            this.LblFrom.TabIndex = 1;
+            this.LblFrom.Text = "From:";
+            // 
+            // LblTo
+            // 
+            this.LblTo.AutoSize = true;
+            this.LblTo.Location = new System.Drawing.Point(26, 167);
+            this.LblTo.Name = "LblTo";
+            this.LblTo.Size = new System.Drawing.Size(23, 13);
+            this.LblTo.TabIndex = 2;
+            this.LblTo.Text = "To:";
+            // 
+            // LblSubject
+            // 
+            this.LblSubject.AutoSize = true;
+            this.LblSubject.Location = new System.Drawing.Point(26, 232);
+            this.LblSubject.Name = "LblSubject";
+            this.LblSubject.Size = new System.Drawing.Size(46, 13);
+            this.LblSubject.TabIndex = 3;
+            this.LblSubject.Text = "Subject:";
+            // 
+            // LblBody
+            // 
+            this.LblBody.AutoSize = true;
+            this.LblBody.Location = new System.Drawing.Point(26, 311);
+            this.LblBody.Name = "LblBody";
+            this.LblBody.Size = new System.Drawing.Size(34, 13);
+            this.LblBody.TabIndex = 4;
+            this.LblBody.Text = "Body:";
+            // 
+            // lblAtach
+            // 
+            this.lblAtach.AutoSize = true;
+            this.lblAtach.Location = new System.Drawing.Point(26, 371);
+            this.lblAtach.Name = "lblAtach";
+            this.lblAtach.Size = new System.Drawing.Size(64, 13);
+            this.lblAtach.TabIndex = 5;
+            this.lblAtach.Text = "Attachment:";
+            // 
+            // LblAccessKey
+            // 
+            this.LblAccessKey.AutoSize = true;
+            this.LblAccessKey.Location = new System.Drawing.Point(302, 41);
+            this.LblAccessKey.Name = "LblAccessKey";
+            this.LblAccessKey.Size = new System.Drawing.Size(74, 13);
+            this.LblAccessKey.TabIndex = 6;
+            this.LblAccessKey.Text = "AccessKeyID:";
+            // 
+            // LblSecretKey
+            // 
+            this.LblSecretKey.AutoSize = true;
+            this.LblSecretKey.Location = new System.Drawing.Point(302, 107);
+            this.LblSecretKey.Name = "LblSecretKey";
+            this.LblSecretKey.Size = new System.Drawing.Size(94, 13);
+            this.LblSecretKey.TabIndex = 7;
+            this.LblSecretKey.Text = "AccessSecretKey:";
+            // 
+            // CbRegion
+            // 
+            this.CbRegion.FormattingEnabled = true;
+            this.CbRegion.Location = new System.Drawing.Point(29, 57);
+            this.CbRegion.Name = "CbRegion";
+            this.CbRegion.Size = new System.Drawing.Size(204, 21);
+            this.CbRegion.TabIndex = 8;
+            // 
+            // TxtFrom
+            // 
+            this.TxtFrom.Location = new System.Drawing.Point(29, 125);
+            this.TxtFrom.Name = "TxtFrom";
+            this.TxtFrom.Size = new System.Drawing.Size(204, 20);
+            this.TxtFrom.TabIndex = 9;
+            // 
+            // TxtTo
+            // 
+            this.TxtTo.Location = new System.Drawing.Point(29, 183);
+            this.TxtTo.Name = "TxtTo";
+            this.TxtTo.Size = new System.Drawing.Size(204, 20);
+            this.TxtTo.TabIndex = 10;
+            // 
+            // TxtSubject
+            // 
+            this.TxtSubject.Location = new System.Drawing.Point(29, 248);
+            this.TxtSubject.Name = "TxtSubject";
+            this.TxtSubject.Size = new System.Drawing.Size(204, 20);
+            this.TxtSubject.TabIndex = 11;
+            // 
+            // TxtBody
+            // 
+            this.TxtBody.Location = new System.Drawing.Point(29, 327);
+            this.TxtBody.Name = "TxtBody";
+            this.TxtBody.Size = new System.Drawing.Size(204, 20);
+            this.TxtBody.TabIndex = 12;
+            // 
+            // txtAttachment
+            // 
+            this.txtAttachment.Location = new System.Drawing.Point(29, 426);
+            this.txtAttachment.Name = "txtAttachment";
+            this.txtAttachment.Size = new System.Drawing.Size(204, 20);
+            this.txtAttachment.TabIndex = 13;
+            // 
+            // TxtKey
+            // 
+            this.TxtKey.Location = new System.Drawing.Point(305, 58);
+            this.TxtKey.Name = "TxtKey";
+            this.TxtKey.Size = new System.Drawing.Size(204, 20);
+            this.TxtKey.TabIndex = 14;
+            // 
+            // TxtSecret
+            // 
+            this.TxtSecret.Location = new System.Drawing.Point(305, 125);
+            this.TxtSecret.Name = "TxtSecret";
+            this.TxtSecret.Size = new System.Drawing.Size(204, 20);
+            this.TxtSecret.TabIndex = 15;
+            // 
+            // TxtFilePath
+            // 
+            this.TxtFilePath.Location = new System.Drawing.Point(305, 229);
+            this.TxtFilePath.Name = "TxtFilePath";
+            this.TxtFilePath.Size = new System.Drawing.Size(204, 20);
+            this.TxtFilePath.TabIndex = 16;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(305, 177);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(201, 17);
+            this.checkBox1.TabIndex = 17;
+            this.checkBox1.Text = "Check To Get Access Key From .csv";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged_1);
+            // 
+            // ButtonSend
+            // 
+            this.ButtonSend.Location = new System.Drawing.Point(29, 452);
+            this.ButtonSend.Name = "ButtonSend";
+            this.ButtonSend.Size = new System.Drawing.Size(75, 23);
+            this.ButtonSend.TabIndex = 18;
+            this.ButtonSend.Text = "Send";
+            this.ButtonSend.UseVisualStyleBackColor = true;
+            this.ButtonSend.Click += new System.EventHandler(this.ButtonSend_Click);
+            // 
+            // ButtonSearch
+            // 
+            this.ButtonSearch.Location = new System.Drawing.Point(305, 200);
+            this.ButtonSearch.Name = "ButtonSearch";
+            this.ButtonSearch.Size = new System.Drawing.Size(201, 23);
+            this.ButtonSearch.TabIndex = 19;
+            this.ButtonSearch.Text = "Search For CSV File";
+            this.ButtonSearch.UseVisualStyleBackColor = true;
+            this.ButtonSearch.Click += new System.EventHandler(this.ButtonSearch_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // CheckDelegate
+            // 
+            this.CheckDelegate.AutoSize = true;
+            this.CheckDelegate.Location = new System.Drawing.Point(305, 283);
+            this.CheckDelegate.Name = "CheckDelegate";
+            this.CheckDelegate.Size = new System.Drawing.Size(128, 17);
+            this.CheckDelegate.TabIndex = 20;
+            this.CheckDelegate.Text = "Use Delegate Sender";
+            this.CheckDelegate.UseVisualStyleBackColor = true;
+            // 
+            // TxtDelegate
+            // 
+            this.TxtDelegate.Location = new System.Drawing.Point(302, 311);
+            this.TxtDelegate.Name = "TxtDelegate";
+            this.TxtDelegate.Size = new System.Drawing.Size(204, 20);
+            this.TxtDelegate.TabIndex = 21;
+            this.TxtDelegate.Text = "Identity ARN";
+            this.TxtDelegate.Enter += new System.EventHandler(this.TxtDelegate_Enter);
+            // 
+            // ButtonSearchFile
+            // 
+            this.ButtonSearchFile.Location = new System.Drawing.Point(29, 397);
+            this.ButtonSearchFile.Name = "ButtonSearchFile";
+            this.ButtonSearchFile.Size = new System.Drawing.Size(201, 23);
+            this.ButtonSearchFile.TabIndex = 22;
+            this.ButtonSearchFile.Text = "Search For Attachment";
+            this.ButtonSearchFile.UseVisualStyleBackColor = true;
+            this.ButtonSearchFile.Click += new System.EventHandler(this.ButtonSearchFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 487);
+            this.Controls.Add(this.ButtonSearchFile);
+            this.Controls.Add(this.TxtDelegate);
+            this.Controls.Add(this.CheckDelegate);
+            this.Controls.Add(this.ButtonSearch);
+            this.Controls.Add(this.ButtonSend);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.TxtFilePath);
+            this.Controls.Add(this.TxtSecret);
+            this.Controls.Add(this.TxtKey);
+            this.Controls.Add(this.txtAttachment);
+            this.Controls.Add(this.TxtBody);
+            this.Controls.Add(this.TxtSubject);
+            this.Controls.Add(this.TxtTo);
+            this.Controls.Add(this.TxtFrom);
+            this.Controls.Add(this.CbRegion);
+            this.Controls.Add(this.LblSecretKey);
+            this.Controls.Add(this.LblAccessKey);
+            this.Controls.Add(this.lblAtach);
+            this.Controls.Add(this.LblBody);
+            this.Controls.Add(this.LblSubject);
+            this.Controls.Add(this.LblTo);
+            this.Controls.Add(this.LblFrom);
+            this.Controls.Add(this.LblRegion);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblRegion;
+        private System.Windows.Forms.Label LblFrom;
+        private System.Windows.Forms.Label LblTo;
+        private System.Windows.Forms.Label LblSubject;
+        private System.Windows.Forms.Label LblBody;
+        private System.Windows.Forms.Label lblAtach;
+        private System.Windows.Forms.Label LblAccessKey;
+        private System.Windows.Forms.Label LblSecretKey;
+        private System.Windows.Forms.ComboBox CbRegion;
+        private System.Windows.Forms.TextBox TxtFrom;
+        private System.Windows.Forms.TextBox TxtTo;
+        private System.Windows.Forms.TextBox TxtSubject;
+        private System.Windows.Forms.TextBox TxtBody;
+        private System.Windows.Forms.TextBox txtAttachment;
+        private System.Windows.Forms.TextBox TxtKey;
+        private System.Windows.Forms.TextBox TxtSecret;
+        private System.Windows.Forms.TextBox TxtFilePath;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button ButtonSend;
+        private System.Windows.Forms.Button ButtonSearch;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.CheckBox CheckDelegate;
+        private System.Windows.Forms.TextBox TxtDelegate;
+        private System.Windows.Forms.Button ButtonSearchFile;
+    }
+}
+
---
+++ SMTP C-Sharp/Form1.cs
@@ -0,0 +1,269 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace SMTP_C_Sharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            {
+                var comboBox = CbRegion;
+                DataTable dt = new DataTable();
+                dt.Columns.AddRange(new DataColumn[] 
+                { 
+                    new DataColumn("Endpoint", typeof(string)), 
+                    new DataColumn("Region", typeof(string)) 
+                });
+
+                dt.Rows.Add("Choose Region", "Choose Region");
+                dt.Rows.Add("email-smtp.us-east-1.amazonaws.com", "US-East-1");
+                dt.Rows.Add("email-smtp.us-east-2.amazonaws.com", "US-East-2");
+                dt.Rows.Add("email-smtp.us-west-1.amazonaws.com","US-West-1");
+                dt.Rows.Add("email-smtp.us-west-2.amazonaws.com", "US-West-2");                
+                dt.Rows.Add("email-smtp.eu-west-1.amazonaws.com", "EU-West-1");
+                dt.Rows.Add("email-smtp.eu-west-2.amazonaws.com", "EU-West-2");
+                dt.Rows.Add("email-smtp.eu-west-3.amazonaws.com", "EU-West-3");
+                dt.Rows.Add("email-smtp.eu-north-1.amazonaws.com", "EU-North-1");
+                dt.Rows.Add("email-smtp.eu-central-1.amazonaws.com", "EU-Central-1");
+                dt.Rows.Add("email-smtp.ap-northeast-1.amazonaws.com", "AP-NorthEast-1");
+                dt.Rows.Add("email-smtp.ap-northeast-2.amazonaws.com", "AP-NorthEast-2");
+                dt.Rows.Add("email-smtp.ap-northeast-3.amazonaws.com", "AP-NorthEast-3");                
+                dt.Rows.Add("email-smtp.ap-southeast-1.amazonaws.com", "AP-SouthEast-1");
+                dt.Rows.Add("email-smtp.ap-southeast-2.amazonaws.com", "AP-SouthEast-2");                
+                dt.Rows.Add("email-smtp.ap-south-1.amazonaws.com", "AP-South-1");
+                dt.Rows.Add("email-smtp.ca-central-1.amazonaws.com", "CA-Central-1");                
+                dt.Rows.Add("email-smtp.sa-east-1.amazonaws.com", "SA-East-1");
+                dt.Rows.Add("email-smtp.us-gov-west-1.amazonaws.com", "US-GOV-West-1");
+                comboBox.DataSource = dt;
+                comboBox.DisplayMember = "Region";
+                comboBox.ValueMember = "Endpoint";                
+            }
+
+            CbRegion.SelectedIndex = 0;
+            TxtBody.Text = "This is a Test";
+            ButtonSearch.Visible = false;
+            TxtFilePath.Visible = false;
+        }
+        private void ButtonSend_Click(object sender, EventArgs e)
+        {
+            var username = TxtKey.Text;
+            var password = TxtSecret.Text;
+            var host = CbRegion.SelectedValue.ToString();
+            int port = 587;
+            var From = TxtFrom.Text;
+            var sTo = TxtTo.Text;
+            var Subject = TxtSubject.Text;
+            var Body = TxtBody.Text;
+
+            if (checkBox1.Checked == true)
+            {
+                username = getUserName();
+                password = getUserPass();
+            }
+
+            if (CheckTextBoxEmpty(CbRegion.Text, username, password) > 0)
+                return;
+
+            if (CheckEmailStrings(sTo, From, Subject, Body) > 0)
+                return;
+
+            var mailMessage = new System.Net.Mail.MailMessage();
+            mailMessage.From = new System.Net.Mail.MailAddress(From);
+            mailMessage.To.Add(sTo);
+            mailMessage.Subject = Subject;
+            mailMessage.Body = Body;
+
+            try
+            {
+                using (var client = new System.Net.Mail.SmtpClient(host, port))
+                {
+                    client.Credentials = new System.Net.NetworkCredential(username, password);
+                    client.EnableSsl = true;
+                    if (txtAttachment.Text != "")
+                    {
+                        string file = txtAttachment.Text;
+                        System.Net.Mail.Attachment attachment;
+                        attachment = new System.Net.Mail.Attachment(file);
+                        mailMessage.Attachments.Add(attachment);
+                    }
+                    if (CheckDelegate.Checked)
+                    {
+                        string headerARN = TxtDelegate.Text;
+                        mailMessage.Headers.Add("X-SES-SOURCE-ARN", headerARN);
+                        mailMessage.Headers.Add("X-SES-FROM-ARN", headerARN);
+                        mailMessage.Headers.Add("X-SES-RETURN-PATH-ARN", headerARN);
+                    }
+                    client.Send(mailMessage);
+                }
+                MessageBox.Show("Sent successful!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private string getUserPass()
+        {
+            var accesskey = "";
+            string filePath = TxtFilePath.Text;
+            filePath = filePath.Replace("\"", "").Trim();
+            using (TextFieldParser myReader = new TextFieldParser(filePath))
+            {
+                myReader.TextFieldType = FieldType.Delimited;
+                myReader.SetDelimiters(",");
+
+                string[] currentRow;
+                while (!myReader.EndOfData)
+                {
+                    try
+                    {
+                        currentRow = myReader.ReadFields();
+                        accesskey = currentRow[2];
+                    }
+                    catch (Exception ex)
+                    {
+                    }
+                }
+            }
+
+            return accesskey;
+        }
+        private string getUserName()
+        {
+            string accessID = "";
+            string filePath = TxtFilePath.Text;
+
+            filePath = filePath.Replace("\"", "").Trim();
+
+            using (TextFieldParser parser = new TextFieldParser(filePath))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+                string[] currentRow;
+                while (!parser.EndOfData)
+                {
+                    try
+                    {
+                        currentRow = parser.ReadFields();
+                        accessID = currentRow[1];
+                    }
+                    catch (Exception ex)
+                    {
+                    }
+                }
+            }
+
+            return accessID;
+        }
+
+        private void CheckBox1_CheckedChanged_1(object sender, EventArgs e)
+        {
+            TxtFilePath.Visible = true;
+            ButtonSearch.Visible = true;
+        }
+        private void TxtFilePath_GotFocus()
+        {
+            TxtFilePath.Text = "";
+        }
+        private int CheckEmailStrings(string sTo, string sFrom, string sSubject, string sBody)
+        {
+            int count = 0;
+            if (sTo == "")
+            {
+                MessageBox.Show("Please Enter To Address");
+                count = 1;
+                return count;
+            }
+            if (sFrom == "")
+            {
+                MessageBox.Show("Please Enter From Address");
+                count += 1;
+                return count;
+            }
+            if (sSubject == "")
+            {
+                MessageBox.Show("Please Enter Subject");
+                count += 1;
+                return count;
+            }
+            if (sBody == "")
+            {
+                MessageBox.Show("Please Enter Body");
+                count += 1;
+                return count;
+            }
+            return count;
+        }
+        private int CheckTextBoxEmpty(string CbRegion, string userName, string password)
+        {
+            int count = 0;
+            if (CbRegion == "Choose Region")
+            {
+                MessageBox.Show("Please Select Region");
+                count += 1;
+                return count;
+            }
+            if (userName == "")
+            {
+                MessageBox.Show("User Name Is Empty");
+                count += 1;
+                return count;
+            }
+            if (password == "")
+            {
+                MessageBox.Show("Access Secret KeyIs Empty");
+                count += 1;
+                return count;
+            }
+            return count;
+        }
+
+        private void TxtBody_GotFocus(object sender, EventArgs e)
+        {
+            TxtBody.Text = "";
+        }
+
+        private string removeQuotes(string filePath)
+        {
+            filePath.Replace("\"", "").Trim();
+            return filePath;
+        }
+
+        private void checkBox1_CheckedChanged_1(object sender, EventArgs e)
+        {
+            TxtFilePath.Visible = true;
+            ButtonSearch.Visible = true;
+        }
+
+        private void ButtonSearch_Click(object sender, EventArgs e)
+        {
+            if ((openFileDialog1.ShowDialog() == DialogResult.OK))
+                TxtFilePath.Text = openFileDialog1.FileName;
+        }
+
+        private void ButtonSearchFile_Click(object sender, EventArgs e)
+        {
+            if ((openFileDialog1.ShowDialog() == DialogResult.OK))
+                txtAttachment.Text = openFileDialog1.FileName;
+        }
+
+        private void TxtDelegate_Enter(object sender, EventArgs e)
+        {
+            if (TxtDelegate.Text == "Identity ARN")
+                TxtDelegate.Text = "";  
+        }
+        
+    }
+}
